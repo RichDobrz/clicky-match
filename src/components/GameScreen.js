61fd2724e485b0b4847528af473b59200246b436
RichDobrz <rjdobrz@gmail.com>
2018-12-15 21:25:14 -0500

    image display tests

---
+++ src/components/GameScreen.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+function GameScreen() {
+    return (
+       <div className="container">
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+           <div className="img-container"><img className="game-images" src="https://via.placeholder.com/170" /></div>
+       </div> 
+    )
+}
+
+export default GameScreen
